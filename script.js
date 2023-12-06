d97ba3f24907402b3e3554f70ce278d73f20d520
Andre026 <151481279+Andre026@users.noreply.github.com>
2023-12-06 14:56:10 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,63 @@
+let Fname;
+let Pos;
+let idnum;
+let rpd;
+let numberofdaywork;
+let rph;
+let otrate;
+let numberofdayworkot;
+
+
+let sss;
+let philhealth;
+let pagibig;
+let tax;
+let totaldeduction;
+let netincome;
+let gross_income;
+
+
+
+
+
+document.getElementById("CALCULATE").onclick = function(){
+
+    Fname = document.getElementById("Fname").value;
+    document.getElementById("name").innerHTML = Fname;
+
+    Pos = document.getElementById("Pos").value;
+    let showPos = document.getElementById("pos").innerHTML = Pos;
+
+    idnum = document.getElementById("idnum").value;
+    idnum = Number(idnum);
+    document.getElementById("id").innerHTML = idnum;
+
+
+    rpd = document.getElementById("RPD").value;
+    rpd = Number(rpd);
+   document.getElementById("rpd").innerHTML = rpd;
+
+    numberofdaywork = document.getElementById("NUMBEROFDAYWORK").value;
+    numberofdaywork = Number(numberofdaywork);
+    document.getElementById("nodw").innerHTML =  numberofdaywork;
+
+    numberofdayworkot = document.getElementById("NUMBEROFDAYWORKOT").value;
+    numberofdayworkot = Number(numberofdayworkot);
+    document.getElementById("nodwOT").innerHTML =  numberofdayworkot;
+
+    rph = rpd / 8;
+
+    otrate = rpd * .30 + rph;
+    gross_income = (rpd * numberofdaywork) + (otrate * numberofdayworkot);
+
+    tax = gross_income * 0.08;
+    sss = gross_income * 0.05;
+    pagibig = gross_income * 0.04;
+    philhealth = gross_income * 0.02;
+
+    totaldeduction = tax + sss + pagibig + philhealth;
+    netincome = gross_income - totaldeduction;
+
+    const total = document.getElementById("totalIncome").innerHTML = "Net income: " + netincome;
+
+}
